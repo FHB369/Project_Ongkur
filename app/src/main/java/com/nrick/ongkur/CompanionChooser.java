37186653764b3361d66e15578c03b218938a5ac4
Faisal Haque Bappy <hbfaisal66@gmail.com>
2019-04-19 13:15:39 +0600

    VR Linked

--- app/src/main/java/com/nrick/ongkur/CompanionChooser.java
+++ app/src/main/java/com/nrick/ongkur/CompanionChooser.java
@@ -2,18 +2,25 @@ package com.nrick.ongkur;
 
 import android.content.Intent;
 import android.os.Build;
+import android.speech.tts.TextToSpeech;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.linkon.game.UnityPlayerActivity;
 
-public class CompanionChooser extends AppCompatActivity {
+import java.util.Locale;
+
+public class CompanionChooser extends AppCompatActivity  implements TextToSpeech.OnInitListener  {
 
     private Button leftOption, rightOption, character;
     private boolean isChanged;
+    private TextView hiText;
+    private TextToSpeech tts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,9 +29,12 @@ public class CompanionChooser extends AppCompatActivity {
 
         isChanged = false;
 
+        tts = new TextToSpeech(this, this);
+
         leftOption = findViewById(R.id.leftOption);
         rightOption = findViewById(R.id.rightOption);
         character = findViewById(R.id.character);
+        hiText = findViewById(R.id.textView4);
 
         leftOption.setOnClickListener(new View.OnClickListener() {
             @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
@@ -32,11 +42,14 @@ public class CompanionChooser extends AppCompatActivity {
             public void onClick(View v) {
                 if(isChanged){
                     character.setBackground(getDrawable(R.drawable.character_1));
+                    hiText.setText("আমি পিকু, আমার বন্ধু হতে চাইলে আমার ছবির উপর ট্যাপ কর");
                     isChanged = false;
                 }else{
                     character.setBackground(getDrawable(R.drawable.character_2));
+                    hiText.setText("আমি সারা, আমার বন্ধু হতে চাইলে আমার ছবির উপর ট্যাপ কর");
                     isChanged = true;
                 }
+                speakOut();
             }
         });
 
@@ -46,22 +59,70 @@ public class CompanionChooser extends AppCompatActivity {
             public void onClick(View v) {
                 if(isChanged){
                     character.setBackground(getDrawable(R.drawable.character_1));
+                    hiText.setText("আমি পিকু, আমার বন্ধু হতে চাইলে আমার ছবির উপর ট্যাপ কর");
                     isChanged = false;
+                    speakOut();
                 }else{
                     character.setBackground(getDrawable(R.drawable.character_2));
+                    hiText.setText("আমি সারা, আমার বন্ধু হতে চাইলে আমার ছবির উপর ট্যাপ কর");
                     isChanged = true;
+                    speakOut();
                 }
+                speakOut();
             }
         });
 
         character.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent launchIntent = getPackageManager().getLaunchIntentForPackage("com.linkon.game");
-                if (launchIntent != null) {
-                    startActivity(launchIntent);//null pointer check in case package name was not found
-                }
+                Intent intent = new Intent(CompanionChooser.this, PreVrWarning.class);
+                startActivity(intent);
             }
         });
+    }
+
+    @Override
+    public void onDestroy() {
+        // Don't forget to shutdown tts!
+        if (tts != null) {
+            tts.stop();
+            tts.shutdown();
+        }
+        super.onDestroy();
+    }
+
+    @Override
+    public void onInit(int status) {
+
+        if (status == TextToSpeech.SUCCESS) {
+
+            int result = tts.setLanguage(new Locale("bn_BD"));
+
+            if (result == TextToSpeech.LANG_MISSING_DATA
+                    || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Log.e("TTS", "This Language is not supported");
+            } else {
+                speakOut();
+            }
+
+        } else {
+            Log.e("TTS", "Initilization Failed!");
+        }
+
+    }
+
+    private void speakOut() {
+
+        if(isChanged) {
+            String text = "আমি সারা, আমার বন্ধু হতে চাইলে আমার ছবির উপর ট্যাপ কর";
+            if(text!=null) {
+                tts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+            }
+        }else{
+            String text = "আমি পিকু, আমার বন্ধু হতে চাইলে আমার ছবির উপর ট্যাপ কর";
+            if(text!=null) {
+                tts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+            }
+        }
     }
 }
--- app/src/main/java/com/nrick/ongkur/PreVrWarning.java
+++ app/src/main/java/com/nrick/ongkur/PreVrWarning.java
@@ -1,13 +1,30 @@
 package com.nrick.ongkur;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PreVrWarning extends AppCompatActivity {
+
+    private Button start;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pre_vr_warning);
+
+        start = findViewById(R.id.buttonStart);
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent launchIntent = getPackageManager().getLaunchIntentForPackage("com.linkon.game");
+                if (launchIntent != null) {
+                    startActivity(launchIntent);//null pointer check in case package name was not found
+                }
+            }
+        });
     }
 }
